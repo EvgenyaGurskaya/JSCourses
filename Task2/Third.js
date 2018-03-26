9c885203321a1a89f067cc4cad032f6a7f25f03d
Evgenya Gurskaya <evgenya.gurskaya@gmail.com>
2018-03-26 13:59:14 +0300

    Corrections

--- Task2/Third.js
+++ Task2/Third.js
@@ -1,3 +1,26 @@
+var sum=0;
+var res=0;
 var N= prompt("Enter the size of array");
 var s= prompt("Enter s");
-var p= prompt("Enter p(%)");
+var p= (prompt("Enter p(%)"))/100;
+var arr = new Array(N);
+for(var i=0; i<N; i++ ){
+		arr [i] = Math.round(70+50*Math.random());
+	}
+	for (var i=0; i<N-1; i++) {
+		for (var j=0; j<N-1-i; j++) {
+			if (arr[j+1] < arr[j]) {
+			var temp = arr[j+1];
+				arr[j+1] = arr[j]; 
+				arr[j] = temp;
+			}
+        }
+     }
+alert ("Sorted array:" + arr);
+for(var i=0; i<N; i++ ){
+if(arr[i]!=0){
+	sum+=1/arr[i];
+}
+else alert("Error!"); }
+res=N/sum;
+alert("Harmonic average="+res);
