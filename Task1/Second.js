aa51b3a10d33d04ddf5ef1b5df29ccf2d0dbdbe1
Evgenya Gurskaya <evgenya.gurskaya@gmail.com>
2018-03-27 10:28:42 +0300

    Corrections

--- Task1/Second.js
+++ Task1/Second.js
@@ -1,9 +1,9 @@
-var x1 = prompt ("Enter the first number:");
+var x1 = +prompt ("Enter the first number:");
 var z = prompt ("Enter the symbol:");
-var x2 = prompt ("Enter the second number:");
+var x2 = +prompt ("Enter the second number:");
 
 switch(z) {
-	case "+": alert("Result="+(+x1+ +x2));break;
+	case "+": alert("Result="+(x1+x2));break;
 	case "-": alert("Result="+(x1-x2));break;
 	case "*": alert("Result="+(x1*x2));break;
 	case "/": alert("Result="+(x1/x2));break;
