e58166ac6a17d5fb03a700f5a72f13bbeaac989e
Evgenya Gurskaya <evgenya.gurskaya@gmail.com>
2018-04-02 12:46:08 +0300

    Corrections

---
+++ Task4/First.js
@@ -0,0 +1,22 @@
+document.getElementById('1').innerText = location.href;
+document.getElementById('2').innerText = location.host;
+document.getElementById('3').innerText = location.hash;
+document.getElementById('4').innerText = location.port;
+document.getElementById('5').innerText = location.protocol;
+document.getElementById('6').innerText = location.search;
+document.getElementById('7').innerText = history.length;
+document.getElementById('8').innerText = screen.width;
+document.getElementById('9').innerText = screen.height;
+document.getElementById('10').innerText = screen.colorDepth;
+document.getElementById('11').innerText = screen.availWidth;
+document.getElementById('12').innerText = screen.availHeight;
+document.getElementById('13').innerText = screen.orientation;
+document.getElementById('14').innerText = navigator.appCodeName;
+document.getElementById('15').innerText = navigator.appName;
+document.getElementById('16').innerText = navigator.cookieEnabled;
+document.getElementById('17').innerText = navigator.onLine;
+document.getElementById('18').innerText = navigator.userAgent;
+document.getElementById('19').innerText = window.innerWidth;
+document.getElementById('20').innerText = window.innerHeight;
+
+	
