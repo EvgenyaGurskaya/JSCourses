ca0460f281e4b53abe5cfdb8411bf451f3a07e32
Evgenya Gurskaya <evgenya.gurskaya@gmail.com>
2018-04-02 13:47:02 +0300

    Corrections

--- Task4/Second.js
+++ Task4/Second.js
@@ -1,5 +1,5 @@
 var str = prompt ("Enter the phone number");
-var tel = /\+?(375|80)\-?\s?\(?(25|29|33|44)\)?\s?\-?[1-9]{3}\s?\-?\d{2}\s?\-?\d{2}$/;
+var tel = /\+?(375|80)\-?\s?\(?(25|29|33|44)\)?\s?\-?[1-9][0-9]{2}\s?\-?\d{2}\s?\-?\d{2}$/;
 
 var res = str.match(tel);
 if(res!=null){
